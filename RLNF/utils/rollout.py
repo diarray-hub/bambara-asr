75e4b2006ac9631c23ddfd6fcd2abed48742f3f1
yazopie <yazopier@gmail.com>
2026-02-01 09:23:39 +0000

    Uncomment normalization of advantage calculation

--- RLNF/utils/rollout.py
+++ RLNF/utils/rollout.py
@@ -171,7 +171,7 @@ def ppo_group_statistics(
     else:
         # Advantage normalized per audio
         adv = reward - values_old
-        #adv = _normalize_adv(adv, indexes, eps=eps)
+        adv = _normalize_adv(adv, indexes, eps=eps)
 
         # Critic target = mean reward per audio
         rewards_means, _, _ = _mean_mean(reward, indexes, only=False)
